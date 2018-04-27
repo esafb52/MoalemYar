e270a12bdff44c4dd8756ef2bc986817394049f1
Mahdi Hosseini <mahdidvb72@gmail.com>
2018-04-27 20:58:02 +0430

    Added new Button Style Changed UI Improvments UI

--- MoalemYar/Properties/AssemblyInfo.cs
+++ MoalemYar/Properties/AssemblyInfo.cs
@@ -57,5 +57,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1677.0")]
-[assembly: AssemblyFileVersion("1.0.1677.0")]
+[assembly: AssemblyVersion("1.0.1701.0")]
+[assembly: AssemblyFileVersion("1.0.1701.0")]
