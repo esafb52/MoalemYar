077a1e636397152269c74ab5cc3223a1f3aaae6b
Mahdi <mahdidvb72@gmail.com>
2018-08-31 16:00:48 +0430

    Added HijriDate

---
+++ MoalemYar/HijriDate/PersianMonth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoalemYar
+{
+    /// <summary>
+
+    /// اجزای ماه شمسی
+
+    /// </summary>
+
+    public class PersianMonth
+
+    {
+
+        /// <summary>
+
+        /// اولین روز ماه شمسی
+
+        /// </summary>
+
+        public DateTime StartDate { set; get; }
+
+
+
+        /// <summary>
+
+        /// آخرین روز ماه شمسی
+
+        /// </summary>
+
+        public DateTime EndDate { set; get; }
+
+    }
+}
--- MoalemYar/MainWindow.xaml.cs
+++ MoalemYar/MainWindow.xaml.cs
@@ -48,7 +48,6 @@ namespace MoalemYar
         {
             LoadSettings();
             getexHint();
-            
         }
 
         #region Query
