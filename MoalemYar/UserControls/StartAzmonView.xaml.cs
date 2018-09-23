f2bc12471445df01f38a5854a23c3a0abb440256
Mahdi Hosseini <mahdidvb72@gmail.com>
2018-09-23 15:54:05 +0330

    azmon Chart fixed

--- MoalemYar/UserControls/StartAzmonView.xaml.cs
+++ MoalemYar/UserControls/StartAzmonView.xaml.cs
@@ -434,21 +434,5 @@ namespace MoalemYar.UserControls
             swCase3.IsChecked = false;
             swCase4.IsChecked = false;
         }
-
-        private void StackPanel_Checked(object sender, RoutedEventArgs e)
-        {
-            var cb = e.OriginalSource as ToggleButton;
-            if (cb.IsChecked == false)
-            {
-                return;
-            }
-            foreach (var item in ((StackPanel)sender).Children)
-            {
-                if (item != cb)
-                {
-                    ((ToggleButton)item).IsChecked = false;
-                }
-            }
-        }
     }
 }
