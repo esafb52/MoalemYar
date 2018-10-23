aad2b9a8d8f5d615765611c9bbb38aae23ebb623
Mahdi Hosseini <mahdidvb72@gmail.com>
2018-10-23 13:57:00 +0330

    improve growl removed VSCode notification

--- MoalemYar/MainWindow.xaml.cs
+++ MoalemYar/MainWindow.xaml.cs
@@ -8,7 +8,6 @@
 *
 ***********************************************************************************/
 
-using Enterwell.Clients.Wpf.Notifications;
 using HandyControl.Controls;
 using MoalemYar.UserControls;
 using MVVMC;
@@ -27,8 +26,6 @@ namespace MoalemYar
     public partial class MainWindow : WindowBorderless
     {
         internal static MainWindow main;
-        public INotificationMessageManager Manager { get; } = new NotificationMessageManager();
-
         public MainWindow()
         {
             InitializeComponent();
@@ -39,8 +36,6 @@ namespace MoalemYar
             ShowCredentialDialog();
             Growl.SetFlowDirection(PanelMessage, FlowDirection.RightToLeft);
             Growl.SetGrowlPanel(PanelMessage);
-            
-
         }
 
         public void ClearScreen()
@@ -201,8 +196,9 @@ namespace MoalemYar
             //Delete Confirm
             if (NotificationKEY.Equals(AppVariable.Delete_Confirm_KEY))
             {
-                Growl.Ask($"آیا برای حذف {param[1]} {param[0]} اطمینان دارید؟", (closeAction, b) => {
-                    if (b)
+                Growl.Ask($"آیا برای حذف {param[1]} {param[0]} اطمینان دارید؟", isConfirm =>
+                {
+                    if (isConfirm)
                     {
                         switch (param[1])
                         {
@@ -234,31 +230,23 @@ namespace MoalemYar
                                 AddQuestionsView.main.deleteGroup();
                                 break;
                         }
-                        closeAction?.Invoke();
                     }
-                    else
-                    {
-                        closeAction?.Invoke();
-                    }
+                    return true;
                 });
             }
             //Reset Data Confirm
             else if (NotificationKEY.Equals(AppVariable.Reset_Data_Confirm_KEY))
             {
-                Growl.Ask($"آیا برای بازیابی {param[0]} اطمینان دارید؟", (closeAction, b) => {
-                    if (b)
+                Growl.Ask($"آیا برای بازیابی {param[0]} اطمینان دارید؟", isConfirm =>
+                {
+                    if (isConfirm)
                     {
                         if (param[0] == "تنظیمات برنامه")
                             SettingsView.main.resetConfig();
                         else
                             SettingsView.main.resetDatabase();
-
-                        closeAction?.Invoke();
                     }
-                    else
-                    {
-                        closeAction?.Invoke();
-                    }
+                    return true;
                 });
             }
 
@@ -266,18 +254,14 @@ namespace MoalemYar
             else if (NotificationKEY.Equals(AppVariable.Data_Reset_Deleted_KEY))
             {
                 //Todo: change button text to راه اندازی
-                Growl.Ask($"{param[0]} به حالت پیشفرض تغییر یافت، برنامه را دوباره راه اندازی کنید", (closeAction, b) => {
-                    if (b)
+                Growl.Ask($"{param[0]} به حالت پیشفرض تغییر یافت، برنامه را دوباره راه اندازی کنید", isConfirm =>
+                {
+                    if (isConfirm)
                     {
                         Application.Current.Shutdown();
                         System.Windows.Forms.Application.Restart();
-
-                        closeAction?.Invoke();
                     }
-                    else
-                    {
-                        closeAction?.Invoke();
-                    }
+                    return true;
                 });
             }
 
@@ -311,8 +295,9 @@ namespace MoalemYar
                 var navigationService = NavigationServiceProvider.GetNavigationServiceInstance();
 
                 //Todo: change button text to ثبت اطلاعات جدید
-                Growl.Ask("اطلاعاتی در پایگاه داده یافت نشد", (closeAction, b) => {
-                    if (b)
+                Growl.Ask("اطلاعاتی در پایگاه داده یافت نشد", isConfirm =>
+                {
+                    if (isConfirm)
                     {
                         switch (param[0])
                         {
@@ -352,13 +337,8 @@ namespace MoalemYar
                                 AddQuestionsView.main.tabc.SelectedIndex = 0;
                                 break;
                         }
-
-                        closeAction?.Invoke();
                     }
-                    else
-                    {
-                        closeAction?.Invoke();
-                    }
+                    return true;
                 });
             }
 
@@ -433,16 +413,12 @@ namespace MoalemYar
             {
                 if (isAvailableOrSuccess)
                 {
-                    Growl.Ask($"نسخه جدید {param[0]} پیدا شد،همین حالا به آخرین نسخه بروزرسانی کنید", (closeAction, b) => {
-                        if (b)
-                        {
+                    Growl.Ask($"نسخه جدید {param[0]} پیدا شد،همین حالا به آخرین نسخه بروزرسانی کنید", isClosed =>
+                    {
+                        if (isClosed)
                             System.Diagnostics.Process.Start(param[1]);
-                            closeAction?.Invoke();
-                        }
-                        else
-                        {
-                            closeAction?.Invoke();
-                        }
+
+                        return true;
                     });
                 }
                 else
@@ -450,299 +426,6 @@ namespace MoalemYar
                     Growl.Info($"شما از آخرین نسخه {AppVariable.getAppVersion} استفاده می کنید");
                 }
             }
-        }
-
-        public void showNotification(string NotificationKEY, bool isAvailableOrSuccess = false, params string[] param)
-        {
-            var builder = NotificationMessageBuilder.CreateMessage();
-            builder.Manager = this.Manager;
-            builder.Message = this.Manager.Factory.GetMessage();
-            builder.Background(AppVariable.BGBLACK);
-
-            builder.Animates(true);
-            builder.Dismiss().WithDelay(TimeSpan.FromSeconds(AppVariable.NotificationDelay));
-            builder.AnimationInDuration(AppVariable.NotificationAnimInDur);
-            builder.AnimationOutDuration(AppVariable.NotificationAnimOutDur);
-
-            //Delete Confirm
-            if (NotificationKEY.Equals(AppVariable.Delete_Confirm_KEY))
-            {
-                builder.HasBadge("هشدار");
-                builder.Accent(AppVariable.RED);
-                builder.HasHeader($"آیا برای حذف {param[1]} {param[0]} اطمینان دارید؟");
-                builder.Dismiss().WithButton("بله", button =>
-                {
-                    switch (param[1])
-                    {
-                        case "مدرسه":
-                            AddSchoolView.main.deleteSchool();
-                            break;
-
-                        case "دانش آموز":
-                            AddStudentView.main.deleteStudent();
-                            break;
-
-                        case "کاربر":
-                            AddUserView.main.deleteUser();
-                            break;
-
-                        case "حضورغیاب":
-                            AttendancelistView.main.deleteAttendance();
-                            break;
-
-                        case "نمره":
-                            QuestionsListView.main.deleteScore();
-                            break;
-
-                        case "گروه":
-                            AddAzmonGroupView.main.deleteGroup();
-                            break;
-
-                        case "سوال":
-                            AddQuestionsView.main.deleteGroup();
-                            break;
-                    }
-                });
-                builder.Dismiss().WithButton("خیر", button => { });
-            }
-            //Reset Data Confirm
-            else if (NotificationKEY.Equals(AppVariable.Reset_Data_Confirm_KEY))
-            {
-                builder.HasBadge("هشدار");
-                builder.Accent(AppVariable.RED);
-                builder.HasHeader($"آیا برای بازیابی {param[0]} اطمینان دارید؟");
-                builder.Dismiss().WithButton("بله", button =>
-                 {
-                     if (param[0] == "تنظیمات برنامه")
-                         SettingsView.main.resetConfig();
-                     else
-                         SettingsView.main.resetDatabase();
-                 });
-                builder.Dismiss().WithButton("خیر", button => { });
-            }
-
-            //Reset Data Deleted
-            else if (NotificationKEY.Equals(AppVariable.Data_Reset_Deleted_KEY))
-            {
-                builder.Accent(AppVariable.GREEN);
-                builder.HasBadge("اطلاعیه");
-                builder.HasMessage($"{param[0]} به حالت پیشفرض تغییر یافت، برنامه را دوباره راه اندازی کنید");
-                builder.WithButton("راه اندازی", button =>
-                {
-                    Application.Current.Shutdown();
-                    System.Windows.Forms.Application.Restart();
-                });
-            }
-
-            //Password Same
-            else if (NotificationKEY.Equals(AppVariable.Same_Password_KEY))
-            {
-                builder.Accent(AppVariable.RED);
-                builder.HasBadge("هشدار");
-                builder.HasMessage("رمز های عبور باید یکسان باشند");
-                builder.Dismiss().WithButton("باشه", button => { });
-            }
-
-            //Delete Exist
-            else if (NotificationKEY.Equals(AppVariable.Delete_Exist_KEY))
-            {
-                builder.Accent(AppVariable.RED);
-                builder.HasBadge("هشدار");
-                builder.HasMessage($"نمی توان این {param[0]} را حذف کرد، ابتدا {param[1]} این {param[0]} را حذف کنید");
-                builder.Dismiss().WithButton("باشه", button => { });
-            }
-
-            //Azmon
-            else if (NotificationKEY.Equals(AppVariable.Azmon_KEY))
-            {
-                builder.Accent(AppVariable.RED);
-                builder.HasBadge("هشدار");
-                builder.HasMessage("تعداد سوالات وارد شده بیشتر از سوالات موجود است");
-                builder.Dismiss().WithButton("باشه", button => { });
-            }
-
-            //Fill All Data
-            else if (NotificationKEY.Equals(AppVariable.Fill_All_Data_KEY))
-            {
-                builder.Accent(AppVariable.RED);
-                builder.HasBadge("هشدار");
-                builder.HasMessage("لطفا تمام فیلدها را پر کنید");
-                builder.Dismiss().WithButton("باشه", button => { });
-            }
-
-            //No Data
-            else if (NotificationKEY.Equals(AppVariable.No_Data_KEY))
-            {
-                var navigationService = NavigationServiceProvider.GetNavigationServiceInstance();
-
-                builder.Accent(AppVariable.RED);
-                builder.HasBadge("هشدار");
-                builder.HasMessage("اطلاعاتی در پایگاه داده یافت نشد");
-                builder.Dismiss().WithButton("ثبت اطلاعات جدید", button =>
-                {
-                    switch (param[0
-])
-                    {
-                        case "School":
-                            AddSchoolView.main.tabc.SelectedIndex = 0;
-                            break;
-
-                        case "User":
-                            AddUserView.main.tabc.SelectedIndex = 0;
-                            break;
-
-                        case "Student":
-                            AddStudentView.main.tabc.SelectedIndex = 0;
-                            break;
-
-                        case "Attendance":
-                            AttendancelistView.main.tabc.SelectedIndex = 0;
-                            break;
-
-                        case "Question":
-                            navigationService.GetController<UserControls.UserControlsController>().AddStudent();
-                            break;
-
-                        case "Score":
-                            QuestionsListView.main.tabc.SelectedIndex = 0;
-                            break;
-
-                        case "TopStudent":
-                            navigationService.GetController<UserControls.UserControlsController>().Questionslist();
-                            break;
-
-                        case "Group":
-                            AddAzmonGroupView.main.tabc.SelectedIndex = 0;
-                            break;
-
-                        case "AQuestions":
-                            AddQuestionsView.main.tabc.SelectedIndex = 0;
-                            break;
-                    }
-                });
-                builder.Dismiss().WithButton("بیخیال", button => { });
-            }
-
-            //Backup
-            else if (NotificationKEY.Equals(AppVariable.Backup_KEY))
-            {
-                if (isAvailableOrSuccess)
-                {
-                    builder.Accent(AppVariable.GREEN);
-                    builder.HasBadge("اطلاعیه");
-                    builder.HasMessage($"{param[0]} با موفقیت انجام شد");
-                    builder.Dismiss().WithButton("باشه", button => { });
-                }
-                else
-                {
-                    builder.Accent(AppVariable.RED);
-                    builder.HasBadge("هشدار");
-                    builder.HasMessage($"{param[0]} با مشکل مواجه شد");
-                    builder.Dismiss().WithButton("باشه", button => { });
-                }
-            }
-
-            //Circular
-            else if (NotificationKEY.Equals(AppVariable.Recived_Circular_KEY))
-            {
-                if (isAvailableOrSuccess)
-                {
-                    builder.Accent(AppVariable.GREEN);
-                    builder.HasBadge("اطلاعیه");
-                    builder.HasMessage("تمامی بخشنامه ها با موفقیت دریافت شد");
-                    builder.Dismiss().WithButton("باشه", button => { });
-                }
-                else
-                {
-                    builder.Accent(AppVariable.RED);
-                    builder.HasBadge("هشدار");
-                    builder.HasMessage("درحال حاظر سرور در دسترس نیست! لطفا در صورت فعال بودن، VPN خود را غیرفعال کنید");
-                    builder.Dismiss().WithButton("باشه", button => { });
-                }
-            }
-
-            //Update Data
-            else if (NotificationKEY.Equals(AppVariable.Update_Data_KEY))
-            {
-                if (isAvailableOrSuccess)
-                {
-                    builder.Accent(AppVariable.ORANGE);
-                    builder.HasBadge("اطلاعیه");
-                    builder.HasMessage($"{param[1]} {param[0]} با موفقیت ویرایش شد");
-                    builder.Dismiss().WithButton("باشه", button => { });
-                }
-                else
-                {
-                    builder.Accent(AppVariable.RED);
-                    builder.HasBadge("هشدار");
-                    builder.HasMessage($"ویرایش {param[1]} {param[0]} با خطا مواجه شد");
-                    builder.Dismiss().WithButton("دوباره امتحان کنید", button => { });
-                }
-            }
-
-            //Deleted
-            else if (NotificationKEY.Equals(AppVariable.Deleted_KEY))
-            {
-                if (isAvailableOrSuccess)
-                {
-                    builder.Accent(AppVariable.BLUE);
-                    builder.HasBadge("اطلاعیه");
-                    builder.HasMessage($"{param[1]} {param[0]} با موفقیت حذف شد");
-                    builder.Dismiss().WithButton("باشه", button => { });
-                }
-                else
-                {
-                    builder.Accent(AppVariable.RED);
-                    builder.HasBadge("هشدار");
-                    builder.HasMessage($"حذف {param[1]} {param[0]} با خطا مواجه شد");
-                    builder.Dismiss().WithButton("دوباره امتحان کنید", button => { });
-                }
-            }
-
-            //Add Data
-            else if (NotificationKEY.Equals(AppVariable.Add_Data_KEY))
-            {
-                if (isAvailableOrSuccess)
-                {
-                    builder.Accent(AppVariable.GREEN);
-                    builder.HasBadge("اطلاعیه");
-                    builder.HasMessage($"{param[1]} {param[0]} با موفقیت ثبت شد");
-                    builder.Dismiss().WithButton("باشه", button => { });
-                }
-                else
-                {
-                    builder.Accent(AppVariable.RED);
-                    builder.HasBadge("هشدار");
-                    builder.HasMessage($"ثبت {param[1]} {param[0]} با خطا مواجه شد");
-                    builder.Dismiss().WithButton("دوباره امتحان کنید", button => { });
-                }
-            }
-
-            //Update
-            else if (NotificationKEY.Equals(AppVariable.Update_KEY))
-            {
-                if (isAvailableOrSuccess)
-                {
-                    builder.Accent(AppVariable.GREEN);
-                    builder.HasBadge("اطلاعیه");
-                    builder.HasHeader($"نسخه جدید {param[0]} پیدا شد،همین حالا به آخرین نسخه بروزرسانی کنید");
-                    builder.WithButton("ارتقا", button =>
-                    {
-                        System.Diagnostics.Process.Start(param[1]);
-                    });
-                    builder.Dismiss().WithButton("بیخیال", button => { });
-                }
-                else
-                {
-                    builder.Accent(AppVariable.RED);
-                    builder.HasBadge("هشدار");
-                    builder.HasHeader($"شما از آخرین نسخه {AppVariable.getAppVersion} استفاده می کنید");
-                    builder.Dismiss().WithDelay(TimeSpan.FromSeconds(3));
-                    builder.Dismiss().WithButton("تایید", button => { });
-                }
-            }
-
-            builder.Queue();
         }
 
         #endregion "Notification"
