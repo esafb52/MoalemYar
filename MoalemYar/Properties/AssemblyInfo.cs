b55d8b1d20232180aa0d558ad906db194ca655b1
Mahdi Hosseini <mahdidvb72@gmail.com>
2018-11-16 12:04:14 +0330

    fix textbox style

--- MoalemYar/Properties/AssemblyInfo.cs
+++ MoalemYar/Properties/AssemblyInfo.cs
@@ -57,5 +57,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.5715.0")]
-[assembly: AssemblyFileVersion("2.2.5715.0")]
+[assembly: AssemblyVersion("2.2.5719.0")]
+[assembly: AssemblyFileVersion("2.2.5719.0")]
